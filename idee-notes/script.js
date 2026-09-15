9ecf16d8828b4072b335348a50cf43e2fbbe7e10
jflacoume <jflacoume@gmail.com>
2026-09-15 08:24:05 +0200

    Add files via upload

--- idee-notes/script.js
+++ idee-notes/script.js
@@ -1,31 +1,31 @@
-    const choixNotes= document.getElementById("nb-puls");
-    let nombreDeNotes = "5";
-    let iteration = 1;
-    const listeNotes = ["DO", "RE", "MI", "FA", "SOL", "LA", "SI"]
-   
 
-    document.addEventListener('click', function(event) {
-        const bouton = event.target ;
-        const etat = bouton.getAttribute("etat");
+const range = [13, 25, 49, 88];
+const depart = [60, 48, 36, 21];
+let r = 0;
+let noteDep = 60 ;
+let note = 60;
 
-        // Réinitianilisation
-        if (bouton.id === "nb-puls"){
-            nombreDeNotes = parseInt(choixNotes.value);
-       
-            for (let i =  1; i <= 5; i++) {
-                const bt1= document.getElementById(i);
-                bt1.style.backgroundColor = (i < nombreDeNotes +1) ? "cornsilk" : "transparent";
-            }
-        }
-        else if (bouton.id === "recommencer") {
-            location.reload();
-        }
-        else if ((bouton.id === "lance-de" || "de") && iteration <= nombreDeNotes){
-            note = Math.floor(Math.random() * 7);
-            console.log(listeNotes[note]);
-            const bt1= document.getElementById(iteration);
-            bt1.textContent = listeNotes[note];
-            iteration ++;
-        }
-        })
+var selectedPreset=_tone_0000_JCLive_sf2_file;
+var AudioContextFunc = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContextFunc();
+var player=new WebAudioFontPlayer();
+player.loader.decodeAfterLoading(audioContext, '_tone_0000_JCLive_sf2_file');
+	
+document.addEventListener('click', function(event) {
+    const bouton = event.target ;
+	if (bouton.id <= 3) {for (let i = 0; i<4; i++) {
+		const bt1= document.getElementById(i);
+		bt1.style.backgroundColor = "transparent";
+	}
+	bouton.style.backgroundColor = "cornsilk";
+	r = range[bouton.id] ;
+	noteDep = depart[bouton.id] ;
+	
+}
+if (bouton.id === "4" ) {
 
+	note = parseInt(Math.floor(Math.random() * r) + noteDep) ;
+	player.queueWaveTable(audioContext, audioContext.destination, selectedPreset, 0, note, 2);
+}
+if (bouton.id === "5") player.queueWaveTable(audioContext, audioContext.destination, selectedPreset, 0, note, 2);
+})
