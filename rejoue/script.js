f8fab5f4b9233c1fce419d4399f25c73e842668a
jflacoume <jflacoume@gmail.com>
2026-09-19 18:34:46 +0200

    Add files via upload

--- rejoue/script.js
+++ rejoue/script.js
@@ -13,11 +13,15 @@ player.loader.decodeAfterLoading(audioContext, '_tone_0000_JCLive_sf2_file');
 	
 document.addEventListener('click', function(event) {
     const bouton = event.target ;
+	const clavier = document.getElementById("clavier");
 	if (bouton.id <= 3) {for (let i = 0; i<4; i++) {
 		const bt1= document.getElementById(i);
+		const clavier = document.getElementById("clavier");
 		bt1.style.backgroundColor = "transparent";
 	}
 	bouton.style.backgroundColor = "cornsilk";
+	console.log("./assets/piano"+bouton.id+".png");
+	clavier.src= "./assets/piano"+bouton.id+".png";
 	r = range[bouton.id] ;
 	noteDep = depart[bouton.id] ;
 	
